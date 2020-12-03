a3f54bf2d4f5c3e331dab3e04ecedb69ec0a43d9
郑建恒 <1421287137@qq.com>
2020-12-03 15:12:19 +0800

    进度: Java_number类

---
+++ project/YunSuanFu.java
@@ -0,0 +1,31 @@
+public class YunSuanFu {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+
+        System.out.println("a + b = " + (a + b) );
+        System.out.println("a - b = " + (a - b) );
+        System.out.println("a * b = " + (a * b) );
+        System.out.println("b / a = " + (b / a) );
+        System.out.println("b % a = " + (b % a) );
+        System.out.println("c % a = " + (c % a) );
+        System.out.println("a++   = " +  (a++) );
+        System.out.println("a--   = " +  (a--) );
+        // 查看  d++ 与 ++d 的不同
+        System.out.println("d++   = " +  (d++) );
+        System.out.println("++d   = " +  (++d) );
+
+        int [] num_arr = {10, 20, 30, 40};
+
+        for ( int i: num_arr ) {
+            if ( i == 20 ) continue;
+            if ( i == 40 ) break;
+            System.out.print( i );
+        }
+
+    }
+
+}
