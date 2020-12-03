8681a26bb8682af8b9153e324eed2d7ab7b16c5e
郑建恒 <1421287137@qq.com>
2020-12-03 17:59:01 +0800

    进度: java_Date类

--- project/YunSuanFu.java
+++ project/YunSuanFu.java
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat;
+import  java.util.*;
+
 public class YunSuanFu {
 
     public static void main(String[] args) {
@@ -26,6 +29,56 @@ public class YunSuanFu {
             System.out.print( i );
         }
 
+        char char1 = 'a';
+        char[] char2 = {'a', 'b', 'c'};
+
+        String str1 = Character.toString(char1) + "bbbb";
+        String str2 = new String(char2);
+        System.out.println("char1转字符串拼接bbb   = " +  str1 );
+        System.out.println("char2转字符串  = " +  str2 );
+        System.out.println("str1连接str2的两种方式  = " +  str1.concat(str2) +"==="+ (str1+str2) );
+
+        // 数组作为参数是引用传递，在 doIt 方法中可以修改数组的值 。
+        System.out.println("数组作为参数是引用传递，在 doIt 方法中可以修改数组的值 。");
+        int[] myArray = {1, 2, 3, 4, 5};
+        YunSuanFu.doIt(myArray);
+        for (int j : myArray) {
+            System.out.println(j + " ");
+        }
+
+        int[] arr = new int[9];
+        System.out.println(arr[8]);
+
+        // 当前格式化时间
+        Date nowDate = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("当前格式化时间" + ft.format(nowDate));
+
+        // 创建一个代表系统当前日期的Calendar对象
+        System.out.println("创建一个代表系统当前日期的Calendar对象");
+        Calendar ca = Calendar.getInstance();//默认是当前日期
+        //将Calendar对象设置为代表2009年6月12日的
+        ca.set(2009, 5, 12);
+        ca.set(Calendar.HOUR_OF_DAY, 18);
+        ca.set(Calendar.MINUTE, 10);
+        ca.set(Calendar.SECOND, 22);
+
+        System.out.println("将Calendar对象设置为代表2009年5月12日的"
+                + ca.get(Calendar.YEAR)
+                +"-"+ca.get(Calendar.MONTH)
+                +"-"+ca.get(Calendar.DATE));
+        ca.add(Calendar.DATE, 1);
+        System.out.println("然后再加一天" + ca.get(Calendar.YEAR)
+                +"-"+ca.get(Calendar.MONTH)
+                +"-"+ca.get(Calendar.DATE));
+
+    }
+
+    static void doIt( int[] z )
+    {
+        int[] A = z;
+        A[0] = 99;
     }
 
 }
