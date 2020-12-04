015636a183cf301bf323c02787119d1fc8961c33
郑建恒 <1421287137@qq.com>
2020-12-04 14:43:40 +0800

    JAVA进度:文件,IO,重写,重载

--- project/MyIo.java
+++ project/MyIo.java
@@ -1,7 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class MyIo
 {
@@ -13,6 +13,10 @@ public class MyIo
 
     public static void main( String[] args )
     {
+        // 打印命令行参数
+        for (String arg : args) {
+            System.out.println("打印命令行参数： " + arg);
+        }
 
         // 使用 System.in 创建 BufferedReader
         BufferedReader bf = new BufferedReader(
@@ -30,9 +34,16 @@ public class MyIo
         // writeToTerim();
 
         // 读取文件
-        readFile();
-        
+         String[] fileTextArr = readFileByLines("D:\\study\\java\\project\\read.log");
+
+        // 写文件
+         writeToFile(fileTextArr, "D:\\study\\java\\project\\write.log");
+
+        // 接受命令行输入,判断是否输入整数
+//         inputCmd();
+
     }
+
 
     /**
      * 从控制台读取多字符输入
@@ -52,6 +63,7 @@ public class MyIo
             e.printStackTrace();
         }
     }
+
 
     /**
      * 从控制台读取多字符输入2
@@ -100,8 +112,120 @@ public class MyIo
     }
 
 
-    public static void readFile()
-    {
+    /**
+     * 读取文件
+     * String fileName
+     */
+    public static String[] readFileByLines(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        String[] fileTextArr = new String[10];
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 0;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                fileTextArr[line] = tempString;
+                System.out.println("line " + (line+1) + ": " + tempString);
+                line++;
+            }
+            reader.close();
+            System.out.println("文本内容 " + Arrays.toString(fileTextArr));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return fileTextArr;
+    }
+
+
+    /**
+     * 将字符写入文件
+     * String[] fileTextArr
+     * String fileName
+     */
+    public static void writeToFile(String[] fileTextArr ,String fileName) {
+        try {
+
+            File f = new File(fileName);
+
+            System.out.println("返回此抽象路径名的路径名字符串。"+f.toString()+"\r\n"
+            +"返回由此抽象路径名表示的文件或目录的名称。"+f.getName()+"\r\n"
+            +"将此抽象路径名转换为一个路径名字符串。"+f.getPath()+"\r\n");
+            OutputStream fop = new FileOutputStream(f, true);
+            // 构建FileOutputStream对象,文件不存在会自动新建
+
+            OutputStreamWriter out = new OutputStreamWriter(fop, "UTF-8");
+            // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+
+            for ( String text: fileTextArr ) {
+                out.append(text);
+                out.append("\r\n");
+            }
+
+            out.close();
+            // 关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
+
+            fop.close();
+            // 关闭输出流,释放系统资源
+
+        } catch (Exception e) {
+            e.printStackTrace();
+//            System.out.println("error is"+e.getMessage());
+        }
+
+    }
+
+
+    /**
+     * 接受命令行输入,判断是否输入整数
+     */
+    public static void inputCmd() {
+
+        boolean bool = true;
+        Scanner scan = new Scanner(System.in);
+        while (bool) {
+            bool = hasInputInt(scan);
+        }
+
+    }
+
+
+    /**
+     * 判断是否输入整数
+     * Scanner scan
+     * @return boolean
+     */
+    public static boolean hasInputInt( Scanner scan ) {
+        System.out.print("请输入整数：");
+        int n = 0;
+        if (scan.hasNextInt()) {
+            // 判断输入的是否是整数
+            n = scan.nextInt();
+            System.out.println("整数数据：" + n);
+            scan.close();
+            return false;
+        } else {
+            // 输入错误的信息
+            String str = scan.nextLine();
+            System.out.println("输入的不是整数！输入的是："+str);
+            return true;
+        }
+    }
+
+
+    // 它在对象被垃圾收集器析构(回收)之前调用
+    protected  void finalize() {
 
     }
 
